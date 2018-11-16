08abbfd458c6abad9dd463f130402a551922ac0c
Wesley Fonseca <wfonseca18@gmail.com>
2018-11-15 20:56:11 -0500

    Adding functionality for incrementing score.

--- app.js
+++ app.js
@@ -6,39 +6,7 @@ $(document).ready(function () {
   //-button press to increment favorite score in array
 
 
-  // Load the Visualization API and the corechart package.
-  google.charts.load('current', { 'packages': ['corechart'] });
-
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.charts.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function drawChart() {
-
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Beer');
-    data.addColumn('number', 'Liked');
-
-    for (let i = 0; i < 5; i++) {
-      data.addRows([
-        [beerList[i].name + ' ', beerList[i].score]
-      ]);
-    }
-
-    // Set chart options
-    var options = {
-      'title': 'Most Liked Beers',
-      'width': 400,
-      'height': 300
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-  }
+ 
 
 
 
@@ -46,6 +14,50 @@ $(document).ready(function () {
 
 
   const render = function () {
+
+ // Load the Visualization API and the corechart package.
+ google.charts.load('current', { 'packages': ['corechart'] });
+
+ // Set a callback to run when the Google Visualization API is loaded.
+ google.charts.setOnLoadCallback(drawChart);
+
+ // Callback that creates and populates a data table,
+ // instantiates the pie chart, passes in the data and
+ // draws it.
+ function drawChart() {
+
+   // Create the data table.
+   var data = new google.visualization.DataTable();
+   data.addColumn('string', 'Beer');
+   data.addColumn('number', 'Liked');
+
+   for (let i = 0; i < 10; i++) {
+     data.addRows([
+       [beerList[i].name + ' ', beerList[i].score]
+     ]);
+   }
+
+   // Set chart options
+   var options = {
+     'title': 'Most Liked Beers',
+     'width': 500,
+     'height': 400
+   };
+
+   // Instantiate and draw our chart, passing in some options.
+   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+   chart.draw(data, options);
+ }
+
+
+
+
+
+
+
+
+
+
     //clear beerButtons ID=favorites to prevent duplicates and then reload buttons after each new addition to beerList array
     $('#favorites').empty();
     // Create a for-loop to iterate through the letters array.
@@ -68,6 +80,9 @@ $(document).ready(function () {
 
         $('#beer-display').html(html);
 
+        // Increment beer score by 1
+        beerList[i].score++;
+        render();
       });
 
     }
